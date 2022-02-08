d768a3863366a7f48144570b20455da087bfd6fd
Nayal404 <93335142+Nayal404@users.noreply.github.com>
2022-02-08 11:15:48 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function read_mind(){
+    let final_answer = document.getElementById('finalanswer').value;
+    let readmind = (final_answer/10|0)-5;
+    // console.log(readmind);
+    document.getElementById('useramount').innerHTML = "Oh! was this your amount?? <br>->"+readmind;
+}
