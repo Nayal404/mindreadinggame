831bfe802ab62a5dfd3aec4e55621b8c4e48ae44
Nayal404 <93335142+Nayal404@users.noreply.github.com>
2022-02-09 16:39:22 +0545

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function read_mind(){
     let final_answer = document.getElementById('finalanswer').value;
     let readmind = (final_answer/10|0)-5;
     // console.log(readmind);
-    document.getElementById('useramount').innerHTML = "Oh! was this your amount?? <br>->"+readmind;
+    document.getElementById('useramount').innerHTML = "Oh! was this your number you were been thinking of?? <br>->"+readmind;
 }
